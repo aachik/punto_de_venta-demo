96c3b6fbe809a7b7fa9d30d3b2f1fae02954d27e
aachik <abdulhamid@outlook.com>
2015-01-05 12:40:14 -0600

    utilizando POO para darle estructura al programa

--- gui.py
+++ gui.py
@@ -1,10 +1,16 @@
 from tkinter import Tk
 from tkinter import Frame
 
-cuadro = Tk()
+class GUI:
 
-f = Frame(width = 1024, height = 768)
-f.pack()
 
-cuadro.mainloop()
+	def __init__(self):
+		self.cuadro = Tk()
+
+		self.f = Frame(width = 1024, height = 768)
+		self.f.pack()
+		self.cuadro.mainloop()
+
+
+if __name__ == "__main__": GUI()
 
