7a758f736188ec4542a27950509e76d6f96d44d4
dnex <dnex@Quorra>
2015-01-09 17:32:59 -0600

    Dejando solo una lista

--- Gui/Punto de venta.py
+++ Gui/Punto de venta.py
@@ -22,7 +22,7 @@ option.grid(row=0, sticky=W)
 
 #Boton de Cerras sesion
 logoutbutton = Button(pvv, text="Cerrar sesion")
-logoutbutton.grid(row=0, sticky=E,column=4)
+logoutbutton.grid(row=0, sticky=W,column=3)
 
 #El primer frame
 f = Frame(pvv)
@@ -30,46 +30,31 @@ f.grid(row=1)
 
 #scrollbar = Scrollbar(f, orient="vertical")
 
-nombrearticulos = Listbox(f,width=20, height=35, )#yscrollcommand=scrollbar.set)
-nombrearticulos.grid(row=0, column=0)
-nombrearticulos.insert(END, "")
+articulos = Listbox(f,width=70, height=35, )#yscrollcommand=scrollbar.set)
+articulos.grid(row=0, column=0)
+articulos.insert(END, "")
 
 #No se como poner el scroll con grid
 #scrollbar.config(command=nombrearticulos.yview)
 #scrollbar.grid(row=0,column=1, sticky=W)
 
 
-precioarticulos = Listbox(f,width=20, height=35, )#yscrollcommand=scrollbar.set)
-precioarticulos.grid(row=0, column=1)
-precioarticulos.insert(END, "")
-#No se como poner el scroll con grid
-#scrollbar.config(command=precioarticulos.yview)
-#scrollbar.grid(row=0,column=3)
-
-cantidadarticulos = Listbox(f,width=20, height=35,)# yscrollcommand=scrollbar.set)
-cantidadarticulos.grid(row=0, column=anda)
-cantidadarticulos.insert(END, "")
-
-#No se como poner el scroll con grid
-#scrollbar.config(command=nombrearticulos.yview)
-#scrollbar.grid(row=0,column=5)
-
 #La entrada de efectivo
 Label(f, text="Efectivo:").grid(row=1, sticky=W)
 e1 = Entry(f)
-e1.grid(row=1, column=1)
+e1.grid(row=1, column=0)
 
 
 
 #Segundo Frame
 f2 = Frame(pvv)
-f2.grid(row=1, column=2)
+f2.grid(row=1, column=1)
 
 
 #No tengo bien definido con que metodo quiero hacer el total, asi que por ahora lo hice asi
-Label(f2, text="Total:").grid(row=2,column=1)
+Label(f2, text="Total:").grid(row=2,column=0)
 Total = Label(f2, text="Suma")
-Total.grid(row=2,column=2)
+Total.grid(row=2,column=1)
 
 # Trabajo pediente
 #	* Hacerlo mas bonito
